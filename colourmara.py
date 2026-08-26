dd15fdce0fb70dabe6316acc44389713bb4eb257
Medusa-Sh <kanankananana6@gmail.com>
2026-08-27 02:27:52 +0900

    Add files via upload

---
+++ colourmara.py
@@ -0,0 +1,9 @@
+import colorama
+from colorama import Fore, Style
+from textblob import TextBlob
+
+#Initalize colorama for colored output
+colorama.init()
+
+#Emojis for the start of the program
+print(f"{Fore.CYAN}WELCOME TO SENTIMENT SPY!{Style.RESET_ALL}")
